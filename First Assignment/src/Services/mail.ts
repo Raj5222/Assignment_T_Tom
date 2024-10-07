bd6e25eff99d5a530fd7088d37f2a487f8d1459c
Raj Sathvara <raj.sathavara122@gmail.com>
2024-10-07 14:57:44 +0530

    First Assignment

--- First Assignment/src/Services/mail.ts
+++ First Assignment/src/Services/mail.ts
@@ -1,16 +1,17 @@
 import { MailerSend, EmailParams, Recipient, Sender } from "mailersend";
 import * as Handlebars from "handlebars";
 import * as fs from "fs";
-import { admin } from "googleapis/build/src/apis/admin";
 
 const emailConfig = {
-  apiKey:
-    "mlsn.7e0c87c5b96f0bd3d84cbe62d5dcf4f06ee9264e33e900924b264039172233d4",
-  senderEmail: "MS_TiRdpG@trial-o65qngkpj0jlwr12.mlsender.net",
+  apiKey: String(process.env.mailkey),
+  senderEmail: String(process.env.email),
 };
 
 // Load email template
-const emailTemplateSource = fs.readFileSync("src/Services/email-template.hbs", "utf8");
+const emailTemplateSource = fs.readFileSync(
+  "src/Services/email-template.hbs",
+  "utf8"
+);
 const emailTemplate = Handlebars.compile(emailTemplateSource);
 
 export async function sendEmail(
@@ -18,7 +19,7 @@ export async function sendEmail(
   subject: string,
   super_admin: string,
   message: string,
-  link?:string
+  link?: string
 ): Promise<boolean> {
   try {
     // Input validation
